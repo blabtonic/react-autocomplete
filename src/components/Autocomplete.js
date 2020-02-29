d2f67e946912a8da40f3dbb2891630c7aa3b0bf1
Trystan Hendricks <dolkhound@gmx.com>
2020-02-29 17:41:18 -0500

    created constructor state

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class Autocomplete extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            // Active selction index
+            activeSuggestion: 0,
+
+            // Suggestion that match the users input
+            filteredSuggestion: [],
+
+            // Whether or not the suggestion list is shown
+            showSuggestion: false,
+
+            // What the user has entered
+            userInput: ""
+        }
+    }
     render(){
         return(
             <input></input>
