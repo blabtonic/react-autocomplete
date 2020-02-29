d088c3bc3d59ace31d59740ebc46781820cc218a
Trystan Hendricks <dolkhound@gmx.com>
2020-02-29 17:29:31 -0500

    creating Autocomplete component

---
+++ src/components/Autocomplete.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Autocomplete extends Component{
+    render(){
+        return(
+            <input></input>
+        );
+    }
+}
+
+export default Autocomplete;
