1c34df9100373f688c39071686288d196c8313af
Trystan Hendricks <dolkhound@gmx.com>
2020-02-29 19:09:15 -0500

    added suggestion data

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1>Autocomplete Search</h1>
       <h3>Type Product here</h3>
-      <Autocomplete></Autocomplete>
+      <Autocomplete suggestions={["DL4MED", "T4M/DEEB","MINNE9593","53/39NDJU5G","54/39NDJU5G","LMED9434","LMED9434A","LMED9434B","LMED9434C","LMED9434D",]}></Autocomplete>
     </div>
   );
 }
