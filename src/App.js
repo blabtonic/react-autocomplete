631aa9ca7bbbac65f1c3f0588f9139c525c3a01c
Trystan Hendricks <dolkhound@gmx.com>
2020-03-02 20:15:46 -0500

    using AutoSuggestion component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Autocomplete from './components/Autocomplete';
+import AutoSuggestion from './components/AutoSuggestion';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <h1>Autocomplete Search</h1>
       <h3>Type Product here</h3>
       <Autocomplete suggestions={["DL4MED", "T4M/DEEB","MINNE9593","53/39NDJU5G","54/39NDJU5G","LMED9434","LMED9434A","LMED9434B","LMED9434C","LMED9434D",]}></Autocomplete>
+      <br/>
+      <AutoSuggestion></AutoSuggestion>
     </div>
   );
 }
---
+++ src/components/AutoSuggestion.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import Autosuggest from 'react-autosuggest';
+
+// products variable will have to get product info from the API TODO:// Try multidimentional array 
+const products = [
+  {
+    name: "DL4MED"
+  },
+  {
+    name: "MINNE9593"
+  },
+  {
+    name: "53/39NDJU5G"
+  },
+  {
+    name: "54/39NDJU5G"
+  },
+  {
+    name: "LMED9434"
+  },
+  {
+    name: "LMED9434A"
+  },
+  {
+    name: "LMED9434B"
+  },
+  {
+    name: "LMED9434C"
+  },
+  {
+    name: "LMED9434D"
+  },
+];
+
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+  const inputValue = value.trim().toUpperCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : products.filter(part_obj =>
+    part_obj.name.toUpperCase().slice(0, inputLength) === inputValue
+  );
+};
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.name;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div>
+    {suggestion.name}
+  </div>
+);
+
+class AutoSuggestion extends Component {
+  constructor() {
+    super();
+
+    // Autosuggest is a controlled component.
+    // This means that you need to provide an input value
+    // and an onChange handler that updates this value (see below).
+    // Suggestions also need to be provided to the Autosuggest,
+    // and they are initially empty because the Autosuggest is closed.
+    this.state = {
+      value: '',
+      suggestions: []
+    };
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+      placeholder: 'Scan Product',
+      value,
+      onChange: this.onChange
+    };
+
+    // Finally, render it!
+    return (
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
+    );
+  }
+}
+
+export default AutoSuggestion;
